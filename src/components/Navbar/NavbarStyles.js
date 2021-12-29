b88946ea5843dbc0c9ec22f8e3e7d5233571ad06
carnoldcoding <carnold1@ithaca.edu>
2021-12-28 19:15:58 -0500

    Started file organization

---
+++ src/components/Navbar/NavbarStyles.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+
+export const Nav = styled.nav`
+    background: #000;
+    height: 80px;
+    margin-top: 0px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1rem;
+    position: sticky;
+    top: 0;
+    z-index: 10;
+    margin: 0;
+    @media screen and (max-width: 960px) {
+        transition: 0.8s all ease;
+    }
+`;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import {Nav} from './NavbarStyles'
+
+const Navbar = () => {
+    return (
+        <>
+        <Nav>
+            Fuck yeah
+        </Nav>
+        </>
+    )
+}
+
+export default Navbar
