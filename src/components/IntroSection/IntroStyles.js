22b3de8c6f43095c8daf9faae196481ed18c9027
carnoldcoding <carnold1@ithaca.edu>
2022-01-07 02:17:28 -0500

    .About(me) section done. Going to redo the entire section in Wow.js instead of FramerMotion

--- src/components/IntroSection/IntroStyles.js
+++ src/components/IntroSection/IntroStyles.js
@@ -2,23 +2,57 @@ import styled from 'styled-components'
 import { motion } from 'framer-motion'
 
 export const IntroContainer = styled.div`
-    position: relative;
     height: 1000px;
-    width: 100%;
+`
+
+export const HeaderWrapper = styled.div`
     display: flex;
     justify-content: center;
 `
 
-export const HeaderWrapper = styled.div`
-    
-`
-
 export const Header = styled(motion.div)`
+
     color: white;
     font-size: 2rem;
     
     background: transparent;
     @media screen and (max-width: 770px){
-        font-size: 1rem;
+        font-size: 2rem;
     }
+`
+
+export const SubtitleWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+`
+
+export const Subtitle = styled(motion.div)`
+    color: white;
+    margin: 1rem;
+    margin-right: 10rem;
+
+    @media screen and (max-width: 770px){
+        font-size: 1.2rem;
+    }
+`
+export const SubtitleTab = styled(motion.div)`
+    color: white;
+    margin-bottom: 1rem;
+    margin-left: 5rem;
+`
+
+export const CodeBox = styled.div`
+    display: flex;
+    width: 25vw;
+    flex-wrap: wrap;
+
+    @media screen and (max-width: 770px){
+
+    }
+`
+
+export const ColorChange = styled.span`
+    color: ${props => props.color}
 `
--- src/components/IntroSection/index.js
+++ src/components/IntroSection/index.js
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react'
 import {IntroContainer,
-        HeaderWrapper, Header} from './IntroStyles'
+        HeaderWrapper, Header,
+    SubtitleWrapper, Subtitle, ColorChange,
+    SubtitleTab, CodeBox} from './IntroStyles'
 import { useIsMobile } from '../../hooks'
 import {useAnimation } from 'framer-motion'
 import { useInView } from 'react-intersection-observer'
@@ -18,10 +20,12 @@ const IntroSection = () => {
         const headerVariants = isMobile
         ?{
             hidden: {
-                scale: 0
+                x: -700,
+                opacity: 0
             },
             visible: {
-                scale: 1,
+                x: 0,
+                opacity: 1,
                 transition: {
                     duration: 2
                 }
@@ -29,11 +33,73 @@ const IntroSection = () => {
         }
         :{
             hidden: {
-                x: -700
+                x: -700,
+                opacity: 0
             },
             visible: {
                 x: 0,
+                opacity: 1,
                 transition: {
+                    duration: 2
+                }
+            }
+        }
+
+        const subtitleVariants = isMobile
+        ? {
+            hidden: {
+                x: -300,
+                opacity: 0
+            },
+            visible: {
+                x: 0,
+                opacity: 1,
+                transition: {
+                    duration: 2,
+                    delay: 1
+                }
+            }
+        }
+        :{
+            hidden: {
+                x: -700,
+                opacity: 0
+            },
+            visible: {
+                x: 0,
+                opacity: 1,
+                transition: {
+                    delay: 1.5, 
+                    duration: 2
+                }
+            }
+        }
+
+        const tabVariants = isMobile
+        ? {
+            hidden: {
+                x: -300,
+                opacity: 0
+            },
+            visible: {
+                x: 0,
+                opacity: 1,
+                transition: {
+                    duration: 2,
+                    delay: 3
+                }
+            }
+        }
+        :{
+            hidden: {
+                x: -700,
+                opacity: 0
+            },
+            visible: {
+                x: 0,
+                opacity: 1,
+                transition: {
+                    delay: 3, 
                     duration: 2
                 }
             }
@@ -50,7 +116,6 @@ const IntroSection = () => {
     });
 
     useEffect(() => {
-        console.log(inView)
         if (inView){
             controls.start('visible');
         }
@@ -69,9 +134,41 @@ const IntroSection = () => {
                     <Header ref={ref} 
                     animate={controls} 
                     variants={headerVariants}>
-                        Software Engineer | Web Developer
+                        .
+                        <ColorChange color="var(--lightblue)">About</ColorChange>
+                        <ColorChange color="var(--magenta)">(</ColorChange>
+                        me
+                        <ColorChange color="var(--magenta)">)</ColorChange>
                     </Header>
                 </HeaderWrapper>
+
+                <SubtitleWrapper>
+                    <CodeBox>
+                        <Subtitle 
+                        ref={ref}
+                        animate={controls}
+                        variants={subtitleVariants}>
+                        <ColorChange color="var(--lightpink)">Education: </ColorChange>
+                        <ColorChange color="var(--magenta)">{'{'} </ColorChange>
+                        </Subtitle>
+                        <SubtitleTab
+                        ref = {ref}
+                        animate={controls}
+                        variants={tabVariants}
+                        >
+                            <ColorChange color="var(--purple)">College:</ColorChange> "Ithaca College",
+                            <br/>
+                            <br />
+                            <ColorChange color="var(--purple)">Degree:</ColorChange> "B.S in Computer Science",
+                            <br />
+                            <br />
+                            <ColorChange color="var(--purple)">Minor:</ColorChange> "Web Development",
+                            <br/>
+                            <br/>
+                            <ColorChange color="var(--magenta)">{'}'} </ColorChange>
+                        </SubtitleTab>
+                    </CodeBox>
+                </SubtitleWrapper>
             </IntroContainer>
         </>
     )
